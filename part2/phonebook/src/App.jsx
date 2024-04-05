6c36a12c2ce0fba983a10d6aabf8336492a3546b
timcode55 <extramailboxes@gmail.com>
2024-04-05 11:47:40 -0500

    Add server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState("");
   const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("http://localhost:3001/persons");
+      const data = await response.json();
+      console.log(data, "RESPONSE");
+      setPersons(data);
+    }
+    getData();
+  }, []);
 
   function handleNameChange(e) {
     setNewName(e.target.value);
@@ -46,10 +51,10 @@ const App = () => {
     setNewName("");
   }
 
-  const filteredPersons = persons.filter((person) => {
+  const filteredPersons = persons?.filter((person) => {
     return person.name.toLowerCase().includes(filterText.toLowerCase());
   });
-
+  console.log(persons, "PERSONS");
   return (
     <div>
       <h2>Phonebook</h2>
